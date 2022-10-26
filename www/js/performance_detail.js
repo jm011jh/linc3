f38ed09cd79ee6f5d4d4cdabf2836653e93c09a7
jm011jh <jm011jh@gmail.com>
2022-10-26 13:28:48 +0900

    performance detail doing

---
+++ www/js/performance_detail.js
@@ -0,0 +1,7 @@
+$(".perf--sort-optionValue").click(function(){
+    $(".perf--sort-optionValue").removeClass("checked")
+    $(this).addClass("checked")
+})
+const swiper = new Swiper(".perd--others02-list",{
+    
+})
